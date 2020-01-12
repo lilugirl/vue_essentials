280965ba8deea4dbf45c0a95d2675c83a606636b
lilu <lilugirl2005@126.com>
2020-01-12 19:27:01 +0800

    Update 02_defineReactive.js

--- 02_defineReactive.js
+++ 02_defineReactive.js
@@ -1,1 +1,19 @@
-dff
+function defineReactive(data,key,val){
+          let dep = new Dep()
+          Object.defineProperty(data,key,{
+              enumerable:true,
+              configurable:true,
+              get:function(){
+                  dep.depend()
+                  return val;
+              },
+              set:function(newVal){
+                  console.log('set newVal',newVal);
+                  if(val===newVal){
+                      return;
+                  }
+                  val=newVal;
+                 dep.notify()
+              }
+          })
+      };
